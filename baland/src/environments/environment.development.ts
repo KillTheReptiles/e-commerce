cdd0c3551a7475f8ff7ef613a0e8a9f2fc9138a5
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2023-04-15 11:44:39 -0500

    first initial setup with Angular

---
+++ baland/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'baland-dev',
+    appId: '1:1096791737987:web:4c31ef443a8eabdb86bdb6',
+    storageBucket: 'baland-dev.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAJvY-55c7PpCrIB4tL3XKURrbaL4qnFLk',
+    authDomain: 'baland-dev.firebaseapp.com',
+    messagingSenderId: '1096791737987',
+  },};
